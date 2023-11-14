6da1ac6fa3be4f6272f020d4ea45d4202552a63f
Matthew Ranford <matt.ranford16@gmail.com>
2023-11-15 12:24:32 +1300

    Determining if current user signed in with useAuth0

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -1,8 +1,15 @@
 // TODO: import useAuth0 function
+import { useAuth0 } from '@auth0/auth0-react'
 
 const useIsAuthenticated = () => {
   // TODO: call the useAuth0 hook, destructure and return isAuthenticated
-  return true
+
+  // Can write it this way as i first did:
+  // return useAuth0().isAuthenticated
+
+  // Gerard would usually write it like this because (it's important that the hooks are called in the same order each time - so they all line up on the right hand side; like a list of hooks)
+  const { isAuthenticated } = useAuth0()
+  return isAuthenticated
 }
 interface Props {
   children: React.ReactNode
