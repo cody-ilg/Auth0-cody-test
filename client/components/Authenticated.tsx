2985d8b726f2076ce21a231edb2ee1f26bd39325
Daniel Kingston <dnlkingston75@gmail.com>
2023-11-15 11:28:11 +1300

    steps 1 - 7 complete

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -1,8 +1,14 @@
 // TODO: import useAuth0 function
+import { useAuth0 } from '@auth0/auth0-react'
 
 const useIsAuthenticated = () => {
   // TODO: call the useAuth0 hook, destructure and return isAuthenticated
-  return true
+
+  const authentication = useAuth0()
+  console.log(authentication.isAuthenticated)
+  return authentication.isAuthenticated
+
+  // return true
 }
 interface Props {
   children: React.ReactNode
