72323dcf65c2324e3b0ceae0a9737afeca752812
student <student@pop-os.localdomain>
2023-11-15 11:33:16 +1300

    Auth0 setup complete

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="roa-2023-sean"
+    clientId="k9oAoO7uf0avUj9yCsVLsleflMn5hSDI"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
