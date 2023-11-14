f6fc73e5b1dd56c7c733026a1adb615ae65ca882
Raidon <ramiahraidon@gmail.com>
2023-11-15 12:50:16 +1300

    Completed challenge

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,19 +1,21 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
 
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
-  const user = {
-    nickname: 'john.doe',
-  }
+
+  const auth = useAuth0()
+  const user = auth.user
+  console.log(user)
 
   const handleSignOut = () => {
-    console.log('sign out')
+    auth.logout()
   }
 
   const handleSignIn = () => {
-    console.log('sign in')
+    auth.loginWithRedirect()
   }
 
   return (
@@ -21,7 +23,7 @@ function Nav() {
       <NavGroup>
         <IfAuthenticated>
           <NavButton onClick={handleSignOut}>Sign out</NavButton>
-          {user && <p>Signed in as: {user?.nickname}</p>}
+          {user && <p>Signed in as: {user?.name}</p>}
         </IfAuthenticated>
         <IfNotAuthenticated>
           <NavButton onClick={handleSignIn}>Sign in</NavButton>
