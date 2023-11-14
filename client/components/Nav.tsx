c4123337d4c2faf41e0f13d62f44ce9b567a92a7
LJ LinesSmith <lewis.lines.smith@gmail.com>
2023-11-15 11:29:53 +1300

    finished step 7

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,10 @@
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
+  const logout = useAuth0().logout
+  const loginWithRedirect = useAuth0().loginWithRedirect
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
   const user = {
@@ -9,11 +12,11 @@ function Nav() {
   }
 
   const handleSignOut = () => {
-    console.log('sign out')
+    logout()
   }
 
   const handleSignIn = () => {
-    console.log('sign in')
+    loginWithRedirect()
   }
 
   return (
