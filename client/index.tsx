3214b7df2968dd5e29f0c420fc0685bcb571b5f2
Keeley Owen <keeleytowen@gmail.com>
2023-11-15 11:39:08 +1300

    completed to step 5

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="roa-2023-keeley.au.auth0.com"
+    clientId="KarfjHL3nWv1ICNZEWZCcGLfygjw9B7R"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
