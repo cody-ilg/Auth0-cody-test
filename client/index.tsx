73d87bd6e0ff73088624a1e7be51d185fe4088f5
Anthony Santiago <anthony.santiago58@yahoo.com>
2023-11-15 11:56:24 +1300

    finish step 5

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="roa-2023-ton.au.auth0.com"
+    clientId="GICoM9K9rwdYTWwGdRFCiS7asqriF9CE"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
