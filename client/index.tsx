a7688b9206085c32d273c0873de281198c7d0e0a
student <student@pop-os.localdomain>
2023-11-15 15:44:40 +1300

    no errors

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,11 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="
+    dev-l6dqqn32odvbsydr.au.auth0.com"
+    clientId="mcne8lCY5BHk6TcY2SV5gqM6tCDylYZC"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
