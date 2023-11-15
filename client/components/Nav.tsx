553069ba554ae12812ac20e996a0dc142350d30d
Rose Northey <roseanorthey@gmail.com>
2023-11-15 15:54:10 +1300

    pass access token

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -5,7 +5,6 @@ import {useAuth0} from '@auth0/auth0-react'
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
-  console.log(useAuth0())
   const {logout, loginWithRedirect, user} = useAuth0()
 
   const handleSignOut = () => {
