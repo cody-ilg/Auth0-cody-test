e383ade086a871e4f0e2cc415936f0fc2b43db3e
student <student@pop-os.localdomain>
2023-11-15 16:01:18 +1300

    show sign in link

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -1,9 +1,15 @@
 // TODO: import useAuth0 function
+import { useAuth0 } from '@auth0/auth0-react'
+import { truncate } from 'fs'
 
 const useIsAuthenticated = () => {
   // TODO: call the useAuth0 hook, destructure and return isAuthenticated
-  return true
+  const { isAuthenticated } = useAuth0()
+  //returns true if isAuthenticated is true otherwise false
+  //if undefined defaults to false
+  return isAuthenticated === undefined ? false : isAuthenticated
 }
+
 interface Props {
   children: React.ReactNode
 }
