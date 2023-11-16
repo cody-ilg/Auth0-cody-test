e6309cac19f45792dcfd4024ca3ed44cc991f2d6
Murphy Ardern <smurfyman86@gmail.com>
2023-11-16 14:23:48 +1300

    import auth0

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -1,4 +1,5 @@
 // TODO: import useAuth0 function
+import { useAuth0 } from "@auth0/auth0-react"
 
 const useIsAuthenticated = () => {
   // TODO: call the useAuth0 hook, destructure and return isAuthenticated
