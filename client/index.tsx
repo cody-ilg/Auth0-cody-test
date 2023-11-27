e676538969230d218cf7e9b062b421329405cd9b
Andrew Dunlop <andrew.w.dunlop@gmail.com>
2023-11-28 12:48:30 +1300

    Auth0Provider setup complete

--- client/index.tsx
+++ client/index.tsx
@@ -17,10 +17,10 @@ root.render(
    * TODO: replace the empty strings below with your own domain, clientId, and audience
    */
   <Auth0Provider
-    domain=""
-    clientId=""
+    domain="roa-2023-andrew.au.auth0.com"
+    clientId="http://localhost:5173/"
     redirectUri={window.location.origin}
-    audience=""
+    audience="https://fruits/api"
   >
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
