1f3090663c322b48a3af7b5fe89ab0d16b1518d9
student <student@pop-os.localdomain>
2023-11-28 10:42:59 +1300

    Step 10 complete

--- server/auth0.ts
+++ server/auth0.ts
@@ -4,9 +4,8 @@ import { ParamsDictionary } from 'express-serve-static-core'
 import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
-// TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
-const audience = 'https://'
+const domain = 'https://roa-2023-noah.au.auth0.com'
+const audience = 'https://fruits/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
