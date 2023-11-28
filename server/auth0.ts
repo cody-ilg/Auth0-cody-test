98d2cf8f66c54b1ee28f1c0ce6327eeadd0a5801
James Crabtree <jamescrabtreenz17@gmail.com>
2023-11-28 16:01:01 +1300

    This is the working version

--- server/auth0.ts
+++ server/auth0.ts
@@ -5,7 +5,7 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://roa-2023-james.au.auth0.com/'
+const domain = 'https://roa-2023-james.au.auth0.com'
 const audience = 'https://fruits/api'
 
 const checkJwt = jwt({
